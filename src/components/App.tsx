da211091db6691090a0a5b1fd31475380a7a3b8d
bikkubs97 <bikku4444@gmail.com>
2023-08-23 14:13:00 +0530

    new

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 
 import Products from "./Products";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Welcome from "./Welcome";
 
 function App(): JSX.Element {
   return (
     <>
       <Router>
         <Routes>
+        <Route path="/" element={<Welcome />} />
           <Route path="/products" element={<Products />} />
         </Routes>
       </Router>
